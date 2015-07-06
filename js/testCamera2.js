55b1ef56eeb997a2807fbe08aeb9f85d77b6cff7
NicolasBuecher <buecher.nicolas@gmail.com>
2015-07-06 14:17:39 +0200

    Rear Camera

--- js/testCamera2.js
+++ js/testCamera2.js
@@ -58,7 +58,7 @@ var constraints = {
         optional: [{sourceId: audioSource}]
     },
     video: {
-        optional: [{sourceId: videos[1]}]
+        optional: [{sourceId: videos[0]}]
     }
 };
 
