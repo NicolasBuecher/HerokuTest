8528503582a212b601774613d853b8f3ff29f174
NicolasBuecher <buecher.nicolas@gmail.com>
2015-07-07 15:09:45 +0200

    Manipulating video using canvas

---
+++ js/testCamera3.js
@@ -0,0 +1,128 @@
+
+navigator.getUserMedia  = navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
+
+if (typeof MediaStreamTrack === 'undefined'){
+    alert('This browser does not support MediaStreamTrack.\n\nTry Chrome Canary.');
+} else {
+    MediaStreamTrack.getSources(gotSources);
+}
+
+var videos = [];
+
+function gotSources(sourceInfos) {
+
+    var j = 0;
+    for (var i = 0; i !== sourceInfos.length; ++i) {
+        var sourceInfo = sourceInfos[i];
+        var option = document.createElement('option');
+        option.value = sourceInfo.id;
+        if (sourceInfo.kind === 'audio') {
+            option.text = sourceInfo.label || 'microphone ' + (audioSelect.length + 1);
+            audioSelect.appendChild(option);
+            console.log(sourceInfo.label + " + " + sourceInfo.id);
+        } else if (sourceInfo.kind === 'video') {
+            option.text = sourceInfo.label || 'camera ' + (videoSelect.length + 1);
+            videoSelect.appendChild(option);
+            console.log(sourceInfo.label + " + " + sourceInfo.id);
+            videos[j] = sourceInfo.id;
+            j++;
+        } else {
+            console.log('Some other kind of source: ', sourceInfo);
+        }
+    }
+    start();
+}
+
+function processFrame(e) {
+    imgData = e.imageData;
+    width = imgData.width;
+    height = imgData.height;
+    for (var j=3; j < imgData.width; j+=4)
+    {
+        // Set all alpha values to medium opacity
+        imgData.data[j] = 128;
+    }
+    // Create new ImageObject with the modified pixel values
+    var canvas = document.createElement('canvas');
+    ctx = canvas.getContext("2d");
+    newImg = ctx.createImageData(width,height);
+    for (j=0; j < imgData.width; j++)
+    {
+        newImg.data[j] = imgData.data[j];
+    }
+    // ... and do something with the modified image ...
+}
+
+function successCallback(stream) {
+
+    if (stream.getVideoTracks()[0])
+        var videoDevice = stream.getVideoTracks()[0];
+
+    var captureDevice = new ImageCapture(videoDevice);
+
+    if (captureDevice) {
+        captureDevice.onframe = processFrame;
+        captureDevice.grabFrame();
+    }
+    /*
+    window.stream = stream; // make stream available to console
+    videoElement.src = window.URL.createObjectURL(stream);
+    videoElement.play();*/
+}
+
+function errorCallback(error){
+    console.log('navigator.getUserMedia error: ', error);
+    alert("ERROR");
+}
+
+var videoElement = document.getElementById('video');
+var audioSelect = document.getElementById('audioSource');
+var videoSelect = document.getElementById('videoSource');
+
+function start()
+{
+    if (!!window.stream) {
+        videoElement.src = null;
+        window.stream.stop();
+    }
+
+    var audioSource = audioSelect.value;
+    var videoSource = videoSelect.value;
+
+    var constraints = {
+        audio: {
+            optional: [{sourceId: audioSource}]
+        },
+        video: {
+            optional: []
+        }
+    };
+
+    for(var i = 0; i < videos.length; i++)
+    {
+        alert("camera numéro " + (i+1) + " OK");
+    }
+
+    switch ( videos.length )
+    {
+        case 0:
+            alert("Aucune caméra détectée");
+            break;
+        case 1:
+            constraints.video.optional.push({sourceId: videos[0]});
+            alert("Il s'agit certainement de ta webcam, non ?");
+            break;
+        case 2:
+            constraints.video.optional.push({sourceId: videos[1]});
+            alert("Au pif, ce doit être ta caméra externe !");
+            break;
+        default:
+            alert("Trop de choix, je ne sais que choisir !");
+            break;
+    }
+
+    navigator.getUserMedia(constraints, successCallback, errorCallback);
+}
