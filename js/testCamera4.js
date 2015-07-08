8191d09bae059fcdf6e2b3f08ee00adcf6f27585
NicolasBuecher <buecher.nicolas@gmail.com>
2015-07-08 12:18:47 +0200

    testCamera

--- js/testCamera4.js
+++ js/testCamera4.js
@@ -1,32 +1,78 @@
+var videoElement = document.getElementById('video');
+
 navigator.getUserMedia  = navigator.getUserMedia ||
     navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia ||
     navigator.msGetUserMedia;
 
-navigator.getUserMedia({video: true}, successCallback, errorCallback);
+if (typeof MediaStreamTrack.getSources(gotSources) === 'undefined')
+{
+    alert('This browser does not support MediaStreamTrack.\n\nTry Chrome Canary.');
+}
+else
+{
+    var videoSources = [];
+    var j = 0;
 
-var videoElement = document.getElementById('video');
+    MediaStreamTrack.getSources(gotSources);
 
-function successCallback(stream) {
+    function gotSources(sourceInfos) {
+        for (var i = 0; i !== sourceInfos.length; ++i) {
+            var sourceInfo = sourceInfos[i];
+            if (sourceInfo.kind === 'video') {
+                videoSources[j] = sourceInfo.id;
+                j++;
+            }
+        }
+    }
 
-    window.stream = stream; // make stream available to console
-    videoElement.src = window.URL.createObjectURL(stream);
-    videoElement.play();
-
-    var tab = stream.getVideoTracks();
-
-    for (var i = 0; i < tab.length; i++)
-    console.log("video " + i);
-
-    setTimeout(end, 6000);
-
-    function end()
-    {
-        videoElement.stop();
+    switch (videos.length) {
+        case 0:
+            alert("Aucune caméra détectée");
+            break;
+        case 1:
+            alert("Il s'agit certainement de ta webcam, non ?");
+            break;
+        case 2:
+            alert("Au pif, ce doit être ta caméra externe !");
+            start(videoSources[1]);
+            break;
+        default:
+            alert("Trop de choix, je ne sais que choisir !");
+            break;
     }
 }
 
-function errorCallback(error){
-    console.log('navigator.getUserMedia error: ', error);
-    alert("ERROR");
+function start(videoId)
+{
+    if (!!window.stream)
+    {
+        videoElement.src = null;
+        window.stream.stop();
+    }
+
+    var constraints = {
+        video: {
+            sourceId: videoId
+        }
+    };
+
+    navigator.getUserMedia(constraints, onSuccessCallback, onErrorCallback);
+
+    function onSuccessCallback(stream)
+    {
+        window.stream = stream;
+        videoElement.src = window.URL.createObjectURL(stream);
+        videoElement.play();
+    }
+
+    function onErrorCallback(error){
+        console.log('navigator.getUserMedia error: ', error);
+        alert("ERROR");
+    }
 }
+
+
+
+
+
